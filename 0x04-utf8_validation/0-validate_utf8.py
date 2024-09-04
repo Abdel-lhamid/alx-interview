a8dccbbd1ecfcd07f2ed9299d704cd40bd4dde88
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-09-04 12:38:08 +0100

    fix code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,14 +3,14 @@
 
 
 def validUTF8(data):
-    """Helper function to check if a byte is a valid continuation byte"""
+    """Determine if a given data set represents a valid UTF-8 encoding."""
     def is_continuation(byte):
+        """Check if a byte is a valid continuation byte."""
         return (byte & 0b11000000) == 0b10000000
 
     i = 0
     while i < len(data):
-        byte = data[i]
-
+        byte = data[i] & 0xFF  # Ensure we only consider the last 8 bits
         if (byte & 0b10000000) == 0:
             i += 1
         elif (byte & 0b11100000) == 0b11000000:
@@ -18,17 +18,18 @@ def validUTF8(data):
                 return False
             i += 2
         elif (byte & 0b11110000) == 0b11100000:
-            if i + 2 >= len(data) or not is_continuation(data[i + 1])
-            or not is_continuation(data[i + 2]):
+            if (i + 2 >= len(data) or
+                    not is_continuation(data[i + 1]) or
+                    not is_continuation(data[i + 2])):
                 return False
             i += 3
         elif (byte & 0b11111000) == 0b11110000:
-            if i + 3 >= len(data) or not is_continuation(data[i + 1])
-            or not is_continuation(data[i + 2])
-            or not is_continuation(data[i + 3]):
+            if (i + 3 >= len(data) or
+                    not is_continuation(data[i + 1]) or
+                    not is_continuation(data[i + 2]) or
+                    not is_continuation(data[i + 3])):
                 return False
             i += 4
         else:
             return False
-
     return True
