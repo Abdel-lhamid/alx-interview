9abbef73f4993d8f760ee6e76797364ad113b346
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-09-20 23:41:20 +0100

    done working tested

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,23 +4,26 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode === 200) {
-    const film = JSON.parse(body);
-    const characters = film.characters;
+if (argc >= 2){
+	const movieId = process.argv[2];
+	request(url, (error, response, body) => {
+		if (error) {
+			console.error('Error:', error);
+		} else if (response.statusCode === 200) {
+			const film = JSON.parse(body);
+			const characters = film.characters;
 
-    characters.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
-      });
-    });
-  } else {
-    console.error(`Failed to retrieve movie data. Status code: ${response.statusCode}`);
-  }
-});
+			characters.forEach((characterUrl) => {
+				request(characterUrl, (error, response, body) => {
+					if (!error && response.statusCode === 200) {
+						const character = JSON.parse(body);
+						console.log(character.name);
+					}
+				});
+			});
+		} else {
+			console.error(`Failed to retrieve movie data. Status code: ${response.statusCode}`);
+		}
+	})
+};
 
